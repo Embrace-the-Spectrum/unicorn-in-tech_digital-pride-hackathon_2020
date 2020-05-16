41e3635d38a531812892604be40dbcfc6b290e94
Xoadra <xijp77@gmail.com>
2020-05-15 19:08:19 -0700

    Basic routing structure established

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
@@ -6,22 +7,31 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className="app">
-            <header className="app-header">
-                <img src={logo} className="app-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="app-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Route exact path="/">
+                <div className="app">
+                    <header className="app-header">
+                        <img src={logo} className="app-logo" alt="logo"/>
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <a
+                            className="app-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn React
+                        </a>
+                    </header>
+                </div>
+            </Route>
+            <Route path="/protest">
+                <div>
+                    <h1>Rawr fite me!</h1>
+                </div>
+            </Route>
+        </BrowserRouter>
     );
 }
 
