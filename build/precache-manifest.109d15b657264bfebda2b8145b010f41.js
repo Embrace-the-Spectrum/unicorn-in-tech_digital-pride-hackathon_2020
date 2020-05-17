a21f098ec76f9258d848573aa33e4fe3302152ad
Kyle Graden <53916246+KyleWitWay@users.noreply.github.com>
2020-05-17 00:43:28 -0700

    fix errors
    to gt a working build

---
+++ build/precache-manifest.109d15b657264bfebda2b8145b010f41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a42c2929836a3c4102041045aad810d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6312d43733969203116",
+    "url": "/static/css/main.5f791398.chunk.css"
+  },
+  {
+    "revision": "4522d409f112cd3e395c",
+    "url": "/static/js/2.755b5cec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.755b5cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6312d43733969203116",
+    "url": "/static/js/main.a8a65907.chunk.js"
+  },
+  {
+    "revision": "bb40985fd8abcde0054d",
+    "url": "/static/js/runtime-main.fa7dd728.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
